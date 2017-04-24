6dcbc9dfa8fc12e80cf96113656fccd5af59cdbc
U-Alexander\Alexander Gai <yg1246@nyu.edu>
2017-04-24 10:17:35 +0800

    assignment 5

---
+++ assignment5/cluster.py
@@ -0,0 +1,3 @@
+from load_data import *
+
+data, labels = load_data()
---
+++ assignment5/load_data.py
@@ -0,0 +1,10 @@
+def load_dataset(path='data'):
+  encoded_labels = {'Iris-setosa' : 0, 'Iris-versicolor' : 1, 'Iris-virginica' : 2}
+  data, labels = [], []
+  for line in open('data', 'r'):
+    fields = line.split(',')
+    data.append(map(float, fields[:-1]))
+    labels.append(encoded_labels[fields[-1])
+
+  import numpy as np
+  return np.array(data), np.array(labels)
